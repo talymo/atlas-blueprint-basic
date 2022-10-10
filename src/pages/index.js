c04732232f16b7f3050b99fef15392aa40b93221
Tabetha Moe <icreatetheinternet@gmail.com>
2022-10-10 15:17:23 -0400

    Added my name

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ export default function Page() {
         <div className="container">
           <section className="hero text-center">
             <Heading className={styles.heading} level="h1">
-              Welcome to your Blueprint
+              Welcome to your Blueprint, Tabby!
             </Heading>
             <p className={styles.description}>
               Achieve unprecedented performance with modern frameworks and the
